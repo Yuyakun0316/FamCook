ceda7656674fd31a4b21b71d486842d0fc58229c
Yuyakun0316 <3.16.yuyakun@gmail.com>
2025-11-21 22:07:41 +0900

    バリデーションの設定

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -1,3 +1,10 @@
 class Meal < ApplicationRecord
   belongs_to :user
+
+  enum meal_type: { breakfast: 0, lunch: 1, dinner: 2, snack: 3 }, _default: :dinner
+
+  validates :title, presence: true, length: { maximum: 50 }
+  validates :description, length: { maximum: 500 }, allow_blank: true
+  validates :date, presence: true
+  validates :meal_type, presence: true
 end
