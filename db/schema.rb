05829e589d706395da22dd426849ed170092ea18
Yuyakun0316 <3.16.yuyakun@gmail.com>
2025-11-27 15:35:24 +0900

    モデル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_11_26_001021) do
+ActiveRecord::Schema[7.1].define(version: 2025_11_27_063233) do
   create_table "active_storage_attachments", charset: "utf8mb3", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -62,6 +62,15 @@ ActiveRecord::Schema[7.1].define(version: 2025_11_26_001021) do
     t.index ["user_id"], name: "index_meals_on_user_id"
   end
 
+  create_table "memos", charset: "utf8mb3", force: :cascade do |t|
+    t.string "category"
+    t.text "content"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_memos_on_user_id"
+  end
+
   create_table "users", charset: "utf8mb3", force: :cascade do |t|
     t.string "name", null: false
     t.string "email", default: "", null: false
@@ -80,4 +89,5 @@ ActiveRecord::Schema[7.1].define(version: 2025_11_26_001021) do
   add_foreign_key "comments", "meals"
   add_foreign_key "comments", "users"
   add_foreign_key "meals", "users"
+  add_foreign_key "memos", "users"
 end
