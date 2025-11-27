af02e65e1e41c09ca8d2301128c8c9e41d6dcf19
Yuyakun0316 <3.16.yuyakun@gmail.com>
2025-11-27 15:49:20 +0900

    コントローラー修正

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -1,4 +1,24 @@
 class MemosController < ApplicationController
+  before_action :authenticate_user!
+
   def index
+    @memos = current_user.memos.order(created_at: :desc)
+    @memo = Memo.new
+  end
+
+  def create
+    @memo = current_user.memos.build(memo_params)
+    if @memo.save
+      redirect_to memos_path, notice: "メモを保存しました✨"
+    else
+      @memos = current_user.memos
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def memo_params
+    params.require(:memo).permit(:category, :content)
   end
 end
