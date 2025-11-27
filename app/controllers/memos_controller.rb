5c44059fd0469b24b82f1fcefca22d95ab05d536
Yuyakun0316 <3.16.yuyakun@gmail.com>
2025-11-27 19:05:29 +0900

    Rubocopを実行

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -4,22 +4,22 @@ class MemosController < ApplicationController
   def index
     @memo = Memo.new
 
-    if params[:category].present?
-      # ☑ カテゴリ指定あり → 該当メモのみ表示（固定メモ優先）
-      @memos = current_user.memos
-                .where(category: params[:category])
-                .order(pinned: :desc, created_at: :desc)
-    else
-      # 📌 カテゴリ指定なし → 全件表示（固定メモ優先）
-      @memos = current_user.memos
-                .order(pinned: :desc, created_at: :desc)
-    end
+    @memos = if params[:category].present?
+               # ☑ カテゴリ指定あり → 該当メモのみ表示（固定メモ優先）
+               current_user.memos
+                           .where(category: params[:category])
+                           .order(pinned: :desc, created_at: :desc)
+             else
+               # 📌 カテゴリ指定なし → 全件表示（固定メモ優先）
+               current_user.memos
+                           .order(pinned: :desc, created_at: :desc)
+             end
   end
 
   def create
     @memo = current_user.memos.build(memo_params)
     if @memo.save
-      redirect_to memos_path, notice: "メモを保存しました✨"
+      redirect_to memos_path, notice: 'メモを保存しました✨'
     else
       @memos = current_user.memos
       render :index, status: :unprocessable_entity
@@ -29,14 +29,14 @@ class MemosController < ApplicationController
   def destroy
     @memo = current_user.memos.find(params[:id])
     @memo.destroy
-    redirect_to memos_path(category: params[:category]), notice: "メモを削除しました"
+    redirect_to memos_path(category: params[:category]), notice: 'メモを削除しました'
   end
 
   # ⭐️ ピン切り替えアクション
   def toggle_pin
     @memo = current_user.memos.find(params[:id])
-    @memo.update(pinned: !@memo.pinned)  # true ↔ false を切り替え
-    redirect_to memos_path(category: params[:category]), notice: "ピンを更新しました📌"
+    @memo.update(pinned: !@memo.pinned) # true ↔ false を切り替え
+    redirect_to memos_path(category: params[:category]), notice: 'ピンを更新しました📌'
   end
 
   private
