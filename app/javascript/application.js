529135cfcea5e1b9969653d9c2f7ae8171ef168d
Yuyakun0316 <3.16.yuyakun@gmail.com>
2025-11-29 00:00:24 +0900

    JS読み込みファイル名変更

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-import "./memo";
-import "./memos_pin";
+import "./memo.js";
+import "./memos_pin.js";
+import "./memo_delete.js";
