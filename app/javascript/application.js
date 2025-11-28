50b3a213761ff235d8f951f13be0ffdd379397b4
Yuyakun0316 <3.16.yuyakun@gmail.com>
2025-11-29 00:48:48 +0900

    JS指定変更

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-import "./memo.js";
-import "./memos_pin.js";
-import "./memo_delete.js";
+import "memo"
+import "memos_pin"
+import "memo_delete"
