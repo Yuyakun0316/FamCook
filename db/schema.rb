a100b91be08466af8c6d64e7fa77a0a72769a556
Yuyakun0316 <3.16.yuyakun@gmail.com>
2025-11-29 17:00:34 +0900

    マイグレーション作成（カラムの追加）

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_11_29_063453) do
+ActiveRecord::Schema[7.1].define(version: 2025_11_29_075713) do
   create_table "active_storage_attachments", charset: "utf8mb3", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -79,6 +79,8 @@ ActiveRecord::Schema[7.1].define(version: 2025_11_29_063453) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "pinned", default: false, null: false
+    t.integer "family_id"
+    t.index ["family_id"], name: "index_memos_on_family_id"
     t.index ["user_id"], name: "index_memos_on_user_id"
   end
 
