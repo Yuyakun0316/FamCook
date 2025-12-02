fce3e211ca0c1c544becdf1018aa504ee0ca63f9
Yuyakun0316 <3.16.yuyakun@gmail.com>
2025-12-02 16:02:17 +0900

    モデル作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,18 @@ class User < ApplicationRecord
   has_many :memos, dependent: :destroy
   attr_accessor :family_code # フォーム入力用（DBに直接保存しない）
 
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
   belongs_to :family, optional: true
+
+  def family_owner?
+    family&.owner_id == id
+  end
+
+  def owned_family
+    Family.find_by(owner_id: id)
+  end
 
   validates :name, presence: true, length: { maximum: 20 }
 end
